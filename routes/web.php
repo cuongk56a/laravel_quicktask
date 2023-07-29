fa3b5bc0e851bd63dbd3d4f33d0bbfe942c2aee2
cuongk56a <laclac10921@gmail.com>
2023-07-29 16:39:37 +0700

    chapter_6

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::resource('users',UserController::class);
+
+Route::prefix('post')->controller(PostController::class)->group(function(){
+    Route::get('/','index')->name('posts.index');
+    Route::get('/create','create')->name('posts.create');
+    Route::post('/','store')->name('posts.store');
+    Route::get('/{post}','show')->name('posts.show');
+    Route::get('/{post}/edit','edit')->name('posts.edit');
+    Route::match(['PUT','PATH'],'/{post}','update')->name('posts.update');
+    Route::delete('/{post}','destroy')->name('posts.destroy');
 });
