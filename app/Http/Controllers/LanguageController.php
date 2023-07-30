f1a8383e1385e3ca3295a37ea064f0cc25429b8c
cuongk56a <laclac10921@gmail.com>
2023-07-31 01:11:28 +0700

    chapter_9

---
+++ app/Http/Controllers/LanguageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Str;
+
+class LanguageController extends Controller
+{
+    public function changeLanguage(Request $request, string $lang) {
+        $supportedLangs = ['en', 'vi'];
+        $lowerLang = Str::lower($lang);
+
+        if (in_array($lowerLang, $supportedLangs)) {
+            Cookie::queue('lang', $lang);
+        }
+
+        return redirect()->back();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,10 +27,25 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::resource('users',UserController::class)->middleware(['admin']);
+
+Route::prefix('post')->controller(PostController::class)->group(function(){
+    Route::get('/','index')->name('posts.index');
+    Route::get('/create','create')->name('posts.create');
+    Route::post('/','store')->name('posts.store');
+    Route::get('/{post}','show')->name('posts.show');
+    Route::get('/{post}/edit','edit')->name('posts.edit');
+    Route::match(['PUT','PATH'],'/{post}','update')->name('posts.update');
+    Route::delete('/{post}','destroy')->name('posts.destroy');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/lang/{lang}', [LanguageController::class, 'changeLanguage'])->name('lang');
 
 require __DIR__.'/auth.php';
