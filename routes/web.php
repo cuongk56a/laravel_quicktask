0012f2580f47b1cb961fbb2eac113e91a41c0614
cuongk56a <laclac10921@gmail.com>
2023-07-30 16:02:43 +0700

    chapter_8

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,14 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('users',UserController::class)->middleware(['admin']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::prefix('post')->controller(PostController::class)->group(function(){
-    Route::get('/','index')->name('posts.index');
-    Route::get('/create','create')->name('posts.create');
-    Route::post('/','store')->name('posts.store');
-    Route::get('/{post}','show')->name('posts.show');
-    Route::get('/{post}/edit','edit')->name('posts.edit');
-    Route::match(['PUT','PATH'],'/{post}','update')->name('posts.update');
-    Route::delete('/{post}','destroy')->name('posts.destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
